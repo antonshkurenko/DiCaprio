ad576f7b608bc5dbd63d20552aa09aec9fbdaaf4
CullyCross <elaugfein@gmail.com>
2016-02-17 13:23:33 +0200

    Add slow param to buff

--- core/src/oscar/dicaprio/scene/actors/buffs/RelativeSlowBuff.java
+++ core/src/oscar/dicaprio/scene/actors/buffs/RelativeSlowBuff.java
@@ -18,17 +18,20 @@ public class RelativeSlowBuff extends RelativeActionBuff {
 
   private static final String TAG = RelativeSlowBuff.class.getSimpleName();
 
+  private final Vector2 mMaxSlow;
+
   //region Constructors
-  public RelativeSlowBuff(RunnerActor actor) {
-    this(actor, 4, Interpolation.linear);
+  public RelativeSlowBuff(RunnerActor actor, Vector2 slow) {
+    this(actor, slow, 4, Interpolation.linear);
   }
 
-  public RelativeSlowBuff(RunnerActor actor, float duration) {
-    this(actor, duration, Interpolation.linear);
+  public RelativeSlowBuff(RunnerActor actor, Vector2 slow, float duration) {
+    this(actor, slow, duration, Interpolation.linear);
   }
 
-  public RelativeSlowBuff(RunnerActor actor, float duration, Interpolation interpolation) {
+  public RelativeSlowBuff(RunnerActor actor, Vector2 slow, float duration, Interpolation interpolation) {
     super(actor, duration, interpolation);
+    mMaxSlow = new Vector2(slow);
   }
   //endregion
 
@@ -36,8 +39,8 @@ public class RelativeSlowBuff extends RelativeActionBuff {
   @Override protected void updateRelative(float percentDelta) {
     try {
       final RunnerUserData runner = ((RunnerActor) actor).getUserData();
-      final Vector2 slow = new Vector2(C.world.runner_max_slow_speed).scl(percentDelta);
-      Gdx.app.log(TAG, "This step slow: " + slow + ", percentDelta: " + percentDelta);
+      final Vector2 slow = new Vector2(mMaxSlow).scl(percentDelta);
+      //Gdx.app.log(TAG, "This step slow: " + slow + ", percentDelta: " + percentDelta);
 
       runner.setBackwardLinearVelocity(runner.getBackwardLinearVelocity().sub(slow));
 
@@ -52,10 +55,9 @@ public class RelativeSlowBuff extends RelativeActionBuff {
     try {
       final RunnerUserData runner = ((RunnerActor) actor).getUserData();
       runner.setBackwardLinearVelocity(
-          runner.getBackwardLinearVelocity().add(C.world.runner_max_slow_speed));
+          runner.getBackwardLinearVelocity().add(mMaxSlow));
 
-      Gdx.app.log(TAG, "Runner speed before slow: " + runner.getLinearVelocity() + ", backward: "
-          + runner.getBackwardLinearVelocity());
+      //Gdx.app.log(TAG, "Runner speed before slow: " + runner.getLinearVelocity() + ", backward: " + runner.getBackwardLinearVelocity());
     } catch (ClassCastException e) {
       throw new RuntimeException("SlowBuff if applicable only to the RunnerActor.");
     }
@@ -67,8 +69,7 @@ public class RelativeSlowBuff extends RelativeActionBuff {
     try {
       final RunnerUserData runner = ((RunnerActor) actor).getUserData();
 
-      Gdx.app.log(TAG, "Runner speed: " + runner.getLinearVelocity() + ", backward: "
-          + runner.getBackwardLinearVelocity());
+      //Gdx.app.log(TAG, "Runner speed: " + runner.getLinearVelocity() + ", backward: " + runner.getBackwardLinearVelocity());
     } catch (ClassCastException e) {
       throw new RuntimeException("SlowBuff if applicable only to the RunnerActor.");
     }
